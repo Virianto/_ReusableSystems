4c142a25382df6596459e1caf016937502671d82
Virianto <victor_nf@outlook.es>
2026-06-23 11:23:23 +0200

    # 1
       - Flag Systems allow developers to keep track about non linear progression

---
+++ Assets/Code/Behaviours/B_FlagSystemImplementation.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+[Flags]
+public enum FlagsExample : short
+{
+    Onboarding = 1 << 0,
+    Step_1 = 1 << 1,
+    Step_2 = 1 << 2,
+    FinalStage = 1 << 3,
+    Credits = 1 << 4,
+
+}
+
+public class B_FlagSystemImplementation : M_FlagSystem<FlagsExample>
+{
+    #region ATTRIBUTES
+
+    public FlagsExample _currentStatus;
+
+    #endregion
+
+    #region METHODS
+ 
+    internal override void SetState(FlagsExample givenFlags)
+    {
+        _currentStatus |= givenFlags;
+    }
+
+    internal override void ClearState(FlagsExample givenFlags)
+    {
+        _currentStatus &= ~givenFlags;
+    }
+
+    internal override void ToggleState(FlagsExample givenFlags)
+    {
+        _currentStatus ^= givenFlags;
+    }
+
+    internal override void SwitchStates(FlagsExample settingFlags, FlagsExample clearingFlags)
+    {
+        _currentStatus |= settingFlags;
+        _currentStatus &= ~clearingFlags;
+    }
+
+    internal override void ClearAllStates()
+    {
+        _currentStatus = 0;
+    }
+
+    internal override bool CheckState(FlagsExample givenFlags)
+    {
+        return _currentStatus.HasFlag(givenFlags);
+    }
+    
+    #endregion
+}
