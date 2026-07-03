1babc18161bfd8793fba5d5f7be03566e85f54aa
Virianto <victor_nf@outlook.es>
2026-07-03 09:22:44 +0200

    # 2

--- Assets/Code/Managers/M_Debug.cs
+++ Assets/Code/Managers/M_Debug.cs
@@ -1,8 +1,18 @@
 using Code.Utils;
 using UnityEngine;
 
+/// <summary>
+/// Debug Manager
+/// This class is responsible for catching all developer debug messages and make them
+/// accessible to any UI through events so the messages are visible outside the editor
+/// </summary>
 public class M_Debug : Singleton<M_Debug>
 {
+    // TO DOs:
+    // - Add a structure for debug messages allowing different types (SO customizable)
+    // - Create necessary events to communicate debug messages to any listener
+    // - Filter, format and build debug messages when needed before launching events
+    
     #region ATTRIBUTES
 
     //public TMP_Text debugText;
