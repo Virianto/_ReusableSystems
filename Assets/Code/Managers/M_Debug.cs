5f29cbace08b5cb351a0b63877c952f5cbe524e4
Virianto <victor_nf@outlook.es>
2026-07-03 09:15:51 +0200

    # 1

--- Assets/Code/Managers/M_Debug.cs
+++ Assets/Code/Managers/M_Debug.cs
@@ -5,13 +5,20 @@ public class M_Debug : Singleton<M_Debug>
 {
     #region ATTRIBUTES
 
-    
+    //public TMP_Text debugText;
+    string _debugString = "";
 
     #endregion
     
     #region METHODS
 
+    public void AddToDebug(string newText)
+    {
+        _debugString += "- " + newText + "\n";
+        //debugText.text = debugString;
 
+        Debug.Log(newText);
+    }
 
     #endregion
 }
