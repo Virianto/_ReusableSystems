0a7e202404f867704f67685e1434ae65c611d83c
Virianto <victor_nf@outlook.es>
2026-07-03 09:05:02 +0200

    · Creating a custom debugging system for cross platform development

---
+++ Assets/Code/Managers/M_Debug.cs
@@ -0,0 +1,17 @@
+using Code.Utils;
+using UnityEngine;
+
+public class M_Debug : Singleton<M_Debug>
+{
+    #region ATTRIBUTES
+
+    
+
+    #endregion
+    
+    #region METHODS
+
+
+
+    #endregion
+}
