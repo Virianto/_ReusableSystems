f558a3ebfbb41b4c9873e26746e408ebc4ff36fd
Virianto <victor_nf@outlook.es>
2026-07-06 10:23:44 +0200

    # M_GlobalInput has Singleton dependency, this is completely optional

--- Assets/Code/Managers/M_GlobalInput.cs
+++ Assets/Code/Managers/M_GlobalInput.cs
@@ -1,19 +1,69 @@
+using System;
+using Code.Utils;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// Global Input Manager
 /// </summary>
-public class M_GlobalInput
+public class M_GlobalInput : Singleton<M_GlobalInput>
 {
     #region ATTRIBUTES
 
-    
+         
+
+    #region Events
+
+    // 'event' declaration removes control from other classes (they can not assign or override)
+    public event Action<InputActionMap> OnInputActionMapChanged;
+
+    #endregion
+
+    public GlobalInputActions globalInputActions;
+
+    public InputControlScheme currentInputControlScheme;
+
+    float horizontalMove = 0;
+    float verticalMove = 0;
+
+    bool isHelpActive;
+    bool isQuickHelpActive;
 
     #endregion
     
     #region METHODS
 
-    
+    void Awake()
+    {
+        globalInputActions = new GlobalInputActions();
+    }
+
+    void Start()
+    {
+        // ! Aquí habría que leerlo de alguna forma al tener un Input del usuario para estar seguros de qué dispositivo es
+        currentInputControlScheme = globalInputActions.KeyboardMouseScheme;
+    }
+
+    /// <summary>
+    /// Modifies current Action Map active and notifies it to other parts of the code
+    /// </summary>
+    /// <param name="newInputActionMap">Mapa de acciones que pasará a estar activo si no lo estaba ya</param>
+    public void SwitchCurrentActionMap(InputActionMap newInputActionMap)
+    {
+        if (!newInputActionMap.enabled)
+        {
+            // With this line we don't disable the entire asset, but all Maps and Actions at once
+            globalInputActions.Disable();
+
+            // Guarantee that only this Action Map will be active
+            newInputActionMap.Enable();
+
+            // Notify Action Map change to other parts of the code
+            OnInputActionMapChanged?.Invoke(newInputActionMap);
+
+            Debug.Log("Current Action Map " + newInputActionMap);
+        }
+    }
 
     #endregion
 }
