0042066481212a3fff02b1d22c647c5088c743b2
Virianto <victor_nf@outlook.es>
2026-07-23 09:56:58 +0200

    # 1

--- Assets/Code/Managers/M_Audio.cs
+++ Assets/Code/Managers/M_Audio.cs
@@ -1,13 +1,32 @@
+using Code.Utils;
 using UnityEngine;
+using UnityEngine.Audio;
+
+public enum AudioGroup : byte
+{
+    Dialogs,
+    FX,
+    Music,
+    Sounds,
+    Other
+}
 
 /// <summary>
 /// Audio Manager
 /// </summary>
-public class M_Audio
+public class M_Audio : Singleton<M_Audio>
 {
     #region ATTRIBUTES
-
     
+    [Header("External References")]
+
+    [SerializeField] AudioMixer globalAudioMixer;
+
+    [SerializeField] GameObject dialogAudioSourceObject;
+    [SerializeField] GameObject fxAudioSourceObject;
+    [SerializeField] GameObject musicAudioSourceObject;
+    [SerializeField] GameObject soundsAudioSourceObject;
+    [SerializeField] GameObject otherAudioSourceObject;
 
     #endregion
     
